43e9114940c9675697abe9c22e05972041f9d56c
SandeshHegde <119592672+SandeshHegde45@users.noreply.github.com>
2024-01-12 14:58:31 +0530

    port changed,server.js changed to index.js

--- backend/index.js
+++ backend/index.js
@@ -26,7 +26,7 @@ dbConnection();
 
 app.use(errorMiddleware);
 app.listen(4000, () => {
-    console.log(`Server is running on port ${process.env.PORT}`);
+    console.log(`Server is running on port 4000`);
 });
 
 export default app;
