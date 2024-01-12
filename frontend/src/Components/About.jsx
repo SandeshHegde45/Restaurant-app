9f234e89023e797711cdf704a53b3bea52961dce
SandeshHegde <119592672+SandeshHegde45@users.noreply.github.com>
2024-01-12 16:19:46 +0530

    Icon, name and about section with minor changes

--- frontend/src/Components/About.jsx
+++ frontend/src/Components/About.jsx
@@ -11,7 +11,7 @@ const About = () => {
                         <p>The only thing we&apos;re serious about is food.</p>
                     </div>
                     <p className="mid">A customer is the most important visitor on our premises, he is not dependent on us. We are dependent on him. He is not an interruption in our work. He is the purpose of it. He is not an outsider in our business. He is part of it. We are not doing him a favor by serving him. He is doing us a favor by giving us an opportunity to do so</p>
-                    <Link to={"menu"}>Explore Menu <span><HiOutlineArrowNarrowRight /></span></Link>
+                    <Link to={"/"}>Explore Menu <span><HiOutlineArrowNarrowRight /></span></Link>
                 </div>
                 <div className="banner">
                     <img src="/about.png" alt="about" />
